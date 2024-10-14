035a7d91936626f7910628d93e2e1ebadb0c00d5
AghedoEmmanuel <emmaodianose@gmail.com>
2024-10-14 06:50:39 +0100

    consuming the backend

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,13 +1,23 @@
 import HeaderBox from '@/components/HeaderBox'
 import RightSideBar from '@/components/RightSideBar'
 import TotalBalanceBox from '@/components/TotalBalanceBox'
+import { getAccount, getAccounts } from '@/lib/actions/banks.action'
 import { getLoggedInUser } from '@/lib/actions/user.action'
 import React from 'react'
 
-const page = async () => {
+const page = async ({searchParams:{id,page}}:SearchParamProps) => {
 
     const loggedIn = await getLoggedInUser()
 
+    const accounts = await getAccounts({userId:loggedIn.$id})
+
+    if(!accounts) return;
+
+    const accountsData = accounts?.data;
+
+    const appwriteItemId = (id as string) || accountsData[0]?.appwriteItemId
+
+    const account = await getAccount({appwriteItemId})
   return (
     <section className='home'>
         <div className='home-content'>
@@ -19,16 +29,16 @@ const page = async () => {
                subtext='Access and manage your account and transactions efficiently'
                />
                <TotalBalanceBox
-               accounts={[]}
-               totalBanks={2}
-               totalCurrentBalance={2346.50}
+               accounts={accountsData}
+               totalBanks={accounts?.totalBanks}
+               totalCurrentBalance={accounts?.totalCurrentBalance}
                />
             </header>
         </div>
         <RightSideBar
         user={loggedIn}
-        transactions={[]}
-        banks={[{currentBalance:234},{currentBalance:436.55}]}
+        transactions={accounts?.transactions}
+        banks={accountsData?.slice(0,2)}
         />
     </section>
   )
